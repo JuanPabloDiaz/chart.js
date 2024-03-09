cd9204cd7dc57b00005a84f9c513163fbdb7bcf0
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-09 11:13:31 -0500

    delete project for fresh start

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold">About</h1>
-      </div>
-    </>
-  );
-}
--- src/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <div className="bg-gray-200 mt-2 py-4 w-full">
-      <p className="text-center">Develop by Juan Diaz</p>
-    </div>
-  );
-};
