c940be34245c1a14fabdc753eede02460535cb9b
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-10 10:29:55 -0400

    Remove @radix-ui/react-icons dependency and add social media icons to the Footer component

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import React from "react";
 
+import { Api, Github, Linkedin, Twitter } from "../icons";
+
 export const Footer = () => {
   const links = [
     { href: "api/area", text: "Area" },
@@ -14,11 +16,28 @@ export const Footer = () => {
     { href: "api/radial", text: "Radial" },
     { href: "api/funnel", text: "Funnel" },
   ];
+  const socials = [
+    {
+      href: "https://github.com/juanpablodiaz",
+      icon: <Github className="w-4 h-4" />,
+    },
+    {
+      href: "https://www.linkedin.com/in/1diazdev/",
+      icon: <Linkedin className="w-4 h-4" />,
+    },
+    {
+      href: "https://twitter.com/1diazdev",
+      icon: <Twitter className="w-4 h-4" />,
+    },
+  ];
 
   return (
     <div className="mt-2 py-4 w-full">
       <div className="text-center pb-10">
-        <h3 className="text-white/40 pb-5">APIs</h3>
+        <div className="text-white/40 pb-5 flex justify-center">
+          <Api className="w-8 h-8" />
+        </div>
+
         <p className="text-yellow-200/60 p-4">
           Data sources I used to create the charts
         </p>
@@ -29,6 +48,15 @@ export const Footer = () => {
               <li className="mx-12 bg-slate-600 hover:bg-slate-800 rounded-xl transition duration-300 ease-in-out">
                 {link.text}
               </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul className="flex justify-center gap-4 items-center">
+          {socials.map((social, index) => (
+            <Link href={social.href} key={index}>
+              <li>{social.icon}</li>
             </Link>
           ))}
         </ul>
--- src/app/components/Loading/Loading.jsx
+++ src/app/components/Loading/Loading.jsx
@@ -1,10 +1,12 @@
-import { ReloadIcon } from "@radix-ui/react-icons";
+import React from "react";
+
+import { EosIconsLoading } from "../icons";
 
 export const Loading = () => {
   return (
     <div className="bg-indigo-500 flex justify-center items-center text-white font-bold py-2 px-4 rounded">
-      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-      <p>Loading...</p>
+      <EosIconsLoading className="mr-2 h-8 w-8" />
+      <p>Loading Graph...</p>
     </div>
   );
 };
